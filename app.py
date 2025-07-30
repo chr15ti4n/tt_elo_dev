1ba7cca602294f676a111444895c99da2cf02ab9
chr15ti4n <christian@knetschowsky.de>
2025-07-30 14:08:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -698,7 +698,7 @@ if st.session_state.view_mode == "home":
         }
         .stDataFrame table th,
         .stDataFrame table td {
-        font-size: 0.5rem !important;
+        font-size: 0.1rem !important;
         padding: 0.1rem 0.2rem !important;
         white-space: nowrap !important;
         overflow: hidden !important;
